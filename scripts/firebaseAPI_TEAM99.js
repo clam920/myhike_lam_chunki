1029a4604194a7b6d0263848ec08b6525db14471
Chunki Lam <clam193@my.bcit.ca>
2024-02-20 13:00:43 -0800

    costumised index, login and main html

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -1,13 +1,13 @@
 //----------------------------------------
 //  Your web app's Firebase configuration
 //----------------------------------------
-var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+const firebaseConfig = {
+        apiKey: "AIzaSyANl5--o4gu_k8afyh5JDDg3uB8Lsw4TyA",
+        authDomain: "comp1800-202410-a160f.firebaseapp.com",
+        projectId: "comp1800-202410-a160f",
+        storageBucket: "comp1800-202410-a160f.appspot.com",
+        messagingSenderId: "30613212050",
+        appId: "1:30613212050:web:ac4cd4c77c0aa895464f4b"
 };
 
 //--------------------------------------------
